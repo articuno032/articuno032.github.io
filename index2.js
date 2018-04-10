6c3fa782f7cd285bdef2b1e660d5fd2007511307
articuno032 <articuno032@gmail.com>
2018-04-10 14:16:50 -0700

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
 function myFunction(){
     var f = document.createElement("IFRAME");
     var i = document.getElementsByTagName("input")[0].value;
-    f.setAttribute("src","https://"+i+"/");
+    f.setAttribute("src","http://"+i+"/");
     document.body.appendChild(f);
 }
